9cc85d9d1693c8388546b6623745c8864109a1dd
rasul00561 <3841104@gmail.com>
2025-11-04 19:41:12 +0500

    finished unit_4

--- unit_3/app/build.gradle.kts
+++ unit_3/app/build.gradle.kts
@@ -56,4 +56,8 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
+    implementation("androidx.navigation:navigation-compose:2.8.3")
+    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.compose.material3:material3:1.3.0")
 }
