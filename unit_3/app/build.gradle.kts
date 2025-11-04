7c17a1a566e78d7b7574ef48ec26b3258d9e5f8f
rasul00561 <3841104@gmail.com>
2025-11-04 21:22:42 +0500

    finished unit_5

--- unit_3/app/build.gradle.kts
+++ unit_3/app/build.gradle.kts
@@ -60,4 +60,9 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.8.3")
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.compose.material3:material3:1.3.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
 }
